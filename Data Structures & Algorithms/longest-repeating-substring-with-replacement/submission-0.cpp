fa9a79acb5c4f96188aaa8f83cd7cc2e52b6d971
thachdd88 <thachdd88@gmail.com>
2026-06-10 14:03:37 +0700

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) 
+    {
+        int len = s.length();
+        int left{0}, right{0}, maxRepeat{0}, maxLen{0};
+        map<char, int> chSet{};
+        for (right = 0; right < len; right++)
+        {
+            char ch = s[right];
+            chSet[ch] += 1;
+            maxRepeat = max(maxRepeat, chSet[ch]);
+            // printf("char: %c, left: %d, right: %d, count: %d\n", 
+            //     ch, left, right, chSet[ch]);
+            if (right-left+1-maxRepeat <= k) 
+            {
+                if (maxLen < right-left+1) { maxLen = right-left+1; }
+            }
+            else 
+            {
+                chSet[s[left]] -= 1;
+                left++;
+            }
+        }
+        return maxLen;
+    }
+};
