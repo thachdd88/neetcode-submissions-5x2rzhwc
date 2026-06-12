2bb939d09882871879737ad2b02e9bd4cb08e908
thachdd88 <thachdd88@gmail.com>
2026-06-12 11:48:04 +0700

    Add: evaluate-reverse-polish-notation - submission-4

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-4.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        for (const auto &token : tokens)
+        {
+            if (token == "+") 
+            {
+                int op1 = (!numStack_.empty())? numStack_.top() : 0;
+                if (!numStack_.empty()) { numStack_.pop(); }
+                
+                numStack_.top() += op1;
+            }
+            else if (token == "-") 
+            {
+                int op1 = (!numStack_.empty())? numStack_.top() : 0;
+                if (!numStack_.empty()) { numStack_.pop(); }
+
+                numStack_.top() -= op1;
+            }
+            else if (token == "*") 
+            {
+                int op1 = (!numStack_.empty())? numStack_.top() : 0;
+                if (!numStack_.empty()) { numStack_.pop(); }
+
+                numStack_.top() *= op1;
+            }
+            else if (token == "/") 
+            {
+                int op1 = (!numStack_.empty())? numStack_.top() : 0;
+                if (!numStack_.empty()) { numStack_.pop(); }
+
+                numStack_.top() /= op1;
+            }
+            else
+            {
+                numStack_.push(stoi(token));
+            }
+        }
+        return numStack_.top();
+    }
+
+private:
+    stack<int> numStack_{};
+};
