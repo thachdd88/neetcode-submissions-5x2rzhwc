f5755f666019c07c1a02210ed44f46b9c888acd1
thachdd88 <thachdd88@gmail.com>
2026-06-15 14:14:15 +0700

    Add: search-2d-matrix - submission-5

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-5.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) 
+    {   
+        bool found{false};
+        // Search row
+        int start{0}, end{static_cast<int>(matrix.size())-1};
+        int rowIdx = (target >= matrix[end][0])? end : -1;
+        if (rowIdx < 0)
+        {
+            while (end > (start+1))
+            {
+                int middle = (start+end)/2;
+                // printf("Row Idx: %d, %d, %d\n", start, middle, end);
+                if (target < matrix[middle][0]) { end = middle; }
+                else { start = middle; }                     
+            }
+            // printf("Row Idx: %d, %d\n", start, end);
+            if ((target >= matrix[start][0]) && ((start == end) || target <= matrix[end][0])) { rowIdx = start; }            
+        }
+        printf("Row Idx: %d\n", rowIdx);
+
+#if 1
+        if (rowIdx >= 0)
+        {
+            int startCol{0}, endCol{static_cast<int>(matrix[rowIdx].size())-1};
+            while (!found && endCol >= startCol)
+            {
+                // printf("Col Idx: %d, %d\n", startCol, endCol);
+                int middle = (startCol+endCol)/2;
+                if (target == matrix[rowIdx][startCol]) { found = true; }
+                else if (target == matrix[rowIdx][endCol]) { found = true; }
+                else if (target == matrix[rowIdx][middle]) { found = true; }
+                else
+                {
+                    if (target < matrix[rowIdx][middle]) { endCol = middle-1; }
+                    else { startCol = middle+1; }
+                }            
+            }
+        }
+#endif
+        return found;
+    }
+};
