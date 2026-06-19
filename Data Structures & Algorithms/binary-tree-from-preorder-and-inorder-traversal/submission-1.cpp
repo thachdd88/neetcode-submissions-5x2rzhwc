add504d4f9e14a932177c014700cb69296aa49ea
thachdd88 <thachdd88@gmail.com>
2026-06-19 14:56:51 +0700

    Add: binary-tree-from-preorder-and-inorder-traversal - submission-1

---
+++ Data Structures & Algorithms/binary-tree-from-preorder-and-inorder-traversal/submission-1.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) 
+    {
+        preorder_ = preorder;
+        for (int i = 0; i < inorder.size(); i++)
+        {
+            inMap_[inorder[i]] = i;
+        }
+        
+        return buildSub(0, 0, inorder.size());
+    }
+    TreeNode *buildSub(int rootIdx, int inL, int inR)
+    {
+        TreeNode *root = new TreeNode(preorder_[rootIdx]);
+        int inIdx = inMap_[preorder_[rootIdx]];
+        
+        int leftSub{-1};
+        for (int i = rootIdx+1; i < preorder_.size(); i++)
+        {
+            int leftIdx = inMap_[preorder_[i]];
+            if ((leftIdx >= inL) && (leftIdx < inIdx))
+            {
+                leftSub = i;
+                break;
+            }
+        }
+        root->left = (leftSub>=0)? buildSub(leftSub, inL, inIdx) : nullptr; 
+
+        int rightSub{-1};
+        for (int i = inIdx+1; i < preorder_.size(); i++)
+        {
+            int rightIdx = inMap_[preorder_[i]];
+            if ((rightIdx < inR) && (inIdx < rightIdx))
+            {
+                rightSub = i;
+                break;
+            }
+        }
+        root->right = (rightSub>=0)? buildSub(rightSub, inIdx, inR) : nullptr; 
+
+        return root;
+    }
+
+private:
+    map<int, int> inMap_{};
+    vector<int> preorder_;
+};
