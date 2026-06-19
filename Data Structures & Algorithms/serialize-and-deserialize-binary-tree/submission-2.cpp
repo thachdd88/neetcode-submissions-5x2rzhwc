dc7581a444dd768240467920ea08c66d9eb65e17
thachdd88 <thachdd88@gmail.com>
2026-06-19 16:41:25 +0700

    Add: serialize-and-deserialize-binary-tree - submission-2

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-2.cpp
@@ -0,0 +1,70 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) 
+    {
+        queue<TreeNode*> qTree;
+        qTree.push(root);
+        stringstream ss;
+        while (!qTree.empty())
+        {
+            int n = qTree.size();
+            for (int i = 0; i < n; i++)
+            {
+                TreeNode *node = qTree.front();
+                qTree.pop();
+                if (node)
+                {
+                    ss << node->val << ',';
+                    qTree.push(node->left);
+                    qTree.push(node->right);
+                }
+                else { ss << "null,"; }
+            }
+        }
+        string res = ss.str();        
+        return res;
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) 
+    {
+        vector<TreeNode*> nodeList;
+        int sIdx = 0;
+        int idx = data.find(',', sIdx);
+        while (idx != string::npos)
+        {
+            string sub = data.substr(sIdx, idx-sIdx);
+            try
+            {
+                int val = stoi(sub);
+                nodeList.push_back(new TreeNode(val));                
+            }
+            catch (const std::exception &ex) { nodeList.push_back(nullptr); }
+            sIdx = idx+1;
+            idx = data.find(',', sIdx);
+        }
+        int child = 0;
+        for (int i = 0; i < nodeList.size(); i++)
+        {
+            TreeNode *node = nodeList[i];
+            if (!node) { continue; }
+            if (++child < nodeList.size()) { node->left = nodeList[child]; }
+            if (++child < nodeList.size()) { node->right = nodeList[child]; }
+        }
+        return (nodeList.empty())? nullptr : nodeList[0];
+    }
+};
