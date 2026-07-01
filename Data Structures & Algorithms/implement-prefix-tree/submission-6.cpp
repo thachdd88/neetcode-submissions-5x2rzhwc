3132b51b1e0b8b86790c49ebc4b74135766b4010
thachdd88 <thachdd88@gmail.com>
2026-07-01 16:37:42 +0700

    Add: implement-prefix-tree - submission-6

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-6.cpp
@@ -0,0 +1,62 @@
+class PrefixTree 
+{
+    struct TreeNode
+    {
+        int key;        
+        map<int, TreeNode *> nextKeys;   
+        bool hasEnd;    
+
+        TreeNode(int key)
+        : key(key), hasEnd(false) {}
+    };
+public:
+    PrefixTree() : root(new TreeNode(-1)) {}
+    
+    void insert(string word) 
+    {
+        TreeNode *iter = root;
+        printf("Insert: ");
+        for (int i = 0; i < word.length(); i++)
+        {
+            int key = word[i] - 'a';
+            if (iter->nextKeys[key] == nullptr)
+            {
+                printf("%c, ", (char)(key+'a'));
+                iter->nextKeys[key] = new TreeNode(key);
+            }
+            else { printf("[%c], ", (char)(key+'a')); }
+            iter = iter->nextKeys[key];
+        }
+        printf("\n");
+        iter->hasEnd = true;
+    }
+    
+    bool search(string word) 
+    {
+        bool found = true;
+        TreeNode *iter = root;
+        for (int i = 0; found && i < word.length(); i++)
+        {
+            int key = word[i] - 'a';
+            if (iter->nextKeys.find(key) == iter->nextKeys.end()) { found = false; }
+            else { iter = iter->nextKeys[key]; }
+        }
+        return found && iter->hasEnd;
+    }
+    
+    bool startsWith(string prefix) 
+    {      
+        bool found = true;
+        TreeNode *iter = root;
+        for (int i = 0; found && i < prefix.length(); i++)
+        {
+            int key = prefix[i] - 'a';
+            if (iter->nextKeys.find(key) == iter->nextKeys.end()) { found = false; }
+            else { iter = iter->nextKeys[key]; }
+        }
+        return found;
+    }
+
+private:
+    TreeNode *root;
+};
