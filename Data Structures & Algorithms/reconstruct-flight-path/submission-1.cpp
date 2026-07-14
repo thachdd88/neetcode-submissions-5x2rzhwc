47d9f7d7f9c09107d67f3717052e51be57f3fbce
thachdd88 <thachdd88@gmail.com>
2026-07-14 13:30:41 +0700

    Add: reconstruct-flight-path - submission-1

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-1.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<string> findItinerary(vector<vector<string>>& tickets) 
+    {        
+        auto cmp = [](const string &s1, const string &s2) { return s1 > s2; };
+        map<string, priority_queue<string, vector<string>, std::greater<string>>> connections;
+        for (auto &flight : tickets) { connections[flight[0]].push(flight[1]); }
+
+        vector<string> res;
+        stack<string> travel;
+        travel.push("JFK");
+        while (!travel.empty())
+        {
+            auto dest = travel.top();
+            if (!connections[dest].empty())
+            {
+                auto next = connections[dest].top();
+                connections[dest].pop();
+                travel.push(next);
+            }
+            else
+            {
+                res.push_back(dest);
+                travel.pop();
+            }
+        }
+
+        std::reverse(res.begin(), res.end());
+        return res;
+    }
+};
