0e66ee60773f4d843c7c08bc356b996a011b16a6
thachdd88 <thachdd88@gmail.com>
2026-07-28 02:03:46 +0700

    Add: longest-increasing-subsequence - submission-1

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-1.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) 
+    {   
+        n = nums.size();
+        counts = vector<int>(n, -1);
+        countMax(0, nums);
+        int cnt{0};
+        for (int i = 0; i < n; i++)
+        {
+            if (counts[i] == -1) { countMax(i, nums); }
+            cnt = max(cnt, counts[i]);
+        }
+        return cnt;
+    }
+
+    void countMax(int idx, vector<int> &nums)
+    {
+        int cnt{0};
+        for (int i = idx+1; i < n; i++)
+        {
+            if (nums[i] > nums[idx])
+            {
+                if (counts[i] == -1) { countMax(i, nums); }
+                cnt = max(cnt, counts[i]);
+            }
+        }
+        counts[idx] = cnt+1;
+    }
+private:
+    int n{0};
+    vector<int> counts;
+};
