0972c75a20d129b960a9091f3a258092b3ec72b5
thachdd88 <thachdd88@gmail.com>
2026-08-18 15:54:15 +0700

    Add: burst-balloons - submission-1

---
+++ Data Structures & Algorithms/burst-balloons/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) {
+        vector<int> exnum(1, 1);
+        for (auto num : nums) { exnum.push_back(num); }
+        exnum.push_back(1);
+
+        int n = nums.size()+2;
+        vector<vector<int>> maxCoins(n, vector<int>(n, 0));
+        for (int size = 2; size < n; size++) {
+            for (int l = 0; l < n-size; l++) {
+                int r = l + size;     
+                for (int mid = l+1; mid < r; mid++) {
+                    maxCoins[l][r] = max(maxCoins[l][r], 
+                                        maxCoins[l][mid] + maxCoins[mid][r] + exnum[l]*exnum[mid]*exnum[r]);
+                }                
+            }            
+        }      
+        return maxCoins[0][n-1];
+    }
+};
