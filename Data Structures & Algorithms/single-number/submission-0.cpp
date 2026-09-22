1f0406ac476778d33766dcde86c00cc2b771cb58
thachdd88 <thachdd88@gmail.com>
2026-09-22 17:09:40 +0700

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int single{0};
+        for (auto num : nums) { single = single ^ num; }
+        return single;
+    }
+};
